10cb3a57cf879c0d7f411b59e4913028be774d81
Ed Asriyan <asriyan1998@yandex.ru>
2017-07-05 20:31:35 +0300

    Add camera support

--- delaunay_transformations/points_generator.h
+++ delaunay_transformations/points_generator.h
@@ -18,8 +18,8 @@
  * @param points_num Points number.
  * @return 1 if success; otherwise 0.
  */
-int dt_generate_random(IplImage* image, del_point2d_t* points, unsigned int points_num);
+unsigned int dt_generate_random(IplImage* image, del_point2d_t* points, unsigned int points_num);
 
-int dt_generate_points_canny(IplImage* image, del_point2d_t* points, unsigned int points_num);
+unsigned int dt_generate_points_canny(IplImage* image, del_point2d_t* points, unsigned int points_num);
 
 #endif //DELAUNAY_TRIANGULATOR_POINTS_GENERATOR_H
